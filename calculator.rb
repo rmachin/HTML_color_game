01e0d9e56a6d7441730321f3bad2a526e9f5d1a7
Raphael Machin <rmachin75@gmail.com>
2017-05-05 11:48:56 -0400

    Delete calculator.rb

--- calculator.rb
+++
@@ -1,8 +0,0 @@
-puts "How much is the bill ($)?"
-bill = gets.to_f
-x = bill * 20 / 100
-#x = bill.percent_of(20)
-puts "The 20% Tip for your meal is:"
-puts "$#{x}."
-total = bill + x
-puts "The total bill  is: $#{total}0"
